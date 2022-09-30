a483382e459c0a366fcc4e0b1181826766edcc2d
IbrahimTs6 <114315719+IbrahimTs6@users.noreply.github.com>
2022-09-30 18:20:26 +0300

    Add files via upload

---
+++ hw13/js/main.js
@@ -0,0 +1,73 @@
+//создаем константу
+/*const birthday = '14.07.2005';
+//используем тип данных string
+//при попытке изменить содержимое const выдает ошибку
+console.log(birthday);
+
+
+//используем тип данных number
+let p = 3.14;
+console.log(p)
+
+let name = prompt('Имя')
+let addres = prompt('Где живете');
+let phone = prompt('Телефон:');
+
+
+let student = 'Maga Aduev';
+let adres = 'Goygova 17';
+let tele = +79364782621
+console.log(`Студент:${student},адрес:${adres},номер телефона:${tele}`);
+
+//программа
+let $ = 58;
+let stockRu = 3558;
+console.log(stockRu/$);
+
+let a = 5;
+let b = 10;
+b = 9
+c = ++ a + b --;
+console.log(c);
+*/
+
+const a = 7;
+
+if(a<10){
+    console.log('константа меньше 10');
+} else{
+    console.log('константа больше 10')
+}
+
+let aa = 12;
+let bb = 17;
+let cc = 15;
+
+let end = aa < 17 && bb < 17 ? 'yes' : 'no'
+console.log(end);
+
+
+
+let type = 'vn'
+switch(type){
+    case('vn1') :
+    console.log(vn1);
+    break;
+
+    case('vn2') :
+    console.log('vn2');
+    break;
+
+    case('vn') :
+    console.log('vn');
+    break;
+}
+
+
+
+
+
+
+
+
+
